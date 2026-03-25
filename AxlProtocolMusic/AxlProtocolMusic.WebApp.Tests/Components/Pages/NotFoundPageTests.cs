0b2be7275262d35a707b0af3222bdeae3954d97d
Bronze Brown <dhamon.nightstar@gmail.com>
2026-03-25 14:08:21 -0500

    Codex - Add custom not found page
    Create a branded 404 experience using the supplied artwork.
    Update the page markup, styling, routing behavior, and page test coverage.

--- AxlProtocolMusic/AxlProtocolMusic.WebApp.Tests/Components/Pages/NotFoundPageTests.cs
+++ AxlProtocolMusic/AxlProtocolMusic.WebApp.Tests/Components/Pages/NotFoundPageTests.cs
@@ -7,13 +7,22 @@ namespace AxlProtocolMusic.WebApp.Tests.Components.Pages;
 public sealed class NotFoundPageTests
 {
     [Test]
-    public void NotFound_RendersExpectedMessage()
+    public void NotFound_RendersCustom404Content()
     {
         using var context = new BunitContext();
 
         var cut = context.Render<NotFound>();
 
-        Assert.That(cut.Markup, Does.Contain("Not Found"));
-        Assert.That(cut.Markup, Does.Contain("Sorry, the content you are looking for does not exist."));
+        Assert.That(cut.Markup, Does.Contain("404 Error"));
+        Assert.That(cut.Markup, Does.Contain("Page not found"));
+        Assert.That(cut.Markup, Does.Contain("The page you requested does not exist or may have moved."));
+        Assert.That(cut.Markup, Does.Contain("Use the site navigation to get back to the section you were looking for."));
+        Assert.That(cut.Markup, Does.Not.Contain("You Might Like"));
+        Assert.That(cut.Markup, Does.Not.Contain("View Article"));
+        Assert.That(cut.Markup, Does.Not.Contain("View Release"));
+
+        var image = cut.Find("img");
+        Assert.That(image.GetAttribute("src"), Is.EqualTo("/Assets/Misc/404-Graphic.png"));
+        Assert.That(image.GetAttribute("alt"), Is.EqualTo("Stylized 404 graphic"));
     }
 }
